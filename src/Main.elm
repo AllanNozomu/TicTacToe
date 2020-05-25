5db569c64f3175ffe540c06e2615e72b8d87e85b
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-05-24 23:36:58 -0300

    Add better check winner logic

--- src/Main.elm
+++ src/Main.elm
@@ -6,14 +6,20 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String
 
+
+
 --Models
+
 
 type alias Model =
     { board : List (List Char)
     , turn : Char
     , winner : Bool
     , draw : Bool
+    , columns : List (List Char)
+    , diagonals : List (List Char)
     }
+
 
 initModel : ( Model, Cmd msg )
 initModel =
@@ -25,26 +31,39 @@ initModel =
       , turn = 'X'
       , winner = False
       , draw = False
+      , columns =
+            [ [ ' ', ' ', ' ' ]
+            , [ ' ', ' ', ' ' ]
+            , [ ' ', ' ', ' ' ]
+            ]
+      , diagonals =
+            [ [ ' ', ' ', ' ' ]
+            , [ ' ', ' ', ' ' ]
+            ]
       }
     , Cmd.none
     )
 
 
+
 --Update
+
 
 type Msg
     = Clear
     | Place Int Int
 
+
 update : Msg -> ( Model, Cmd msg ) -> ( Model, Cmd msg )
-update msg (model, cmd) =
+update msg ( model, cmd ) =
     case msg of
         Clear ->
             initModel
 
         Place posx posy ->
             if model.winner then
-                (model, Cmd.none)
+                ( model, Cmd.none )
+
             else
                 let
                     newBoard =
@@ -56,123 +75,133 @@ update msg (model, cmd) =
                     isDraw =
                         if newWinner then
                             False
+
                         else
                             checkDraw newBoard
 
                     newTurn =
                         if newWinner then
                             model.turn
+
                         else if model.turn == 'X' then
                             'O'
+
                         else
                             'X'
                 in
-                    ({ model | board = newBoard, turn = newTurn, winner = newWinner, draw = isDraw }, Cmd.none)
+                ( { model
+                    | board = newBoard
+                    , turn = newTurn
+                    , winner = newWinner
+                    , draw = isDraw
+                  }
+                , Cmd.none
+                )
 
 
 checkForWinner : List (List Char) -> Bool
 checkForWinner board =
-    (checkRow board || checkColumn board || checkDiagonals board)
+    checkRow board || checkColumn board || checkDiagonals board
 
 
 checkDraw : List (List Char) -> Bool
 checkDraw board =
-    not <| List.member ' ' (List.foldr List.append [] board)
+    not (List.concat board |> List.member ' ')
 
 
 allEqual : List Char -> Bool
 allEqual list =
-    case List.head list of
-        Just ' ' ->
-            False
+    let
+        value =
+            takeElement 0 list ' '
+    in
+    List.foldr
+        (\element status ->
+            if status && not (element == ' ') then
+                element == value
+            else
+                False
+        )
+        True
+        list
 
-        Just value ->
-            List.foldr
-                (\element status ->
-                    if status then
-                        element == value
-                    else
-                        False
-                )
-                True
-                list
 
+takeElement : Int -> List a -> a -> a
+takeElement index list default =
+    case List.drop index list |> List.head of
+        Just l ->
+            l
         Nothing ->
-            False
-
-
-takeElement : Int -> List a -> List a
-takeElement index list =
-    List.take 1 <| List.drop index list
+            default
 
 
 checkDiagonals : List (List Char) -> Bool
 checkDiagonals board =
     let
         diagonal1 =
-            List.concat <|
-                List.indexedMap
-                    (\index row ->
-                        takeElement index row
-                    )
-                <|
-                    board
+            List.indexedMap
+                (\index row ->
+                    takeElement index row ' '
+                )
+                board
 
         diagonal2 =
-            List.concat <|
-                List.indexedMap
-                    (\index row ->
-                        takeElement (2 - index) row
-                    )
-                <|
-                    board
+            List.indexedMap
+                (\index row ->
+                    takeElement (2 - index) row ' '
+                )
+                board
     in
-        (allEqual diagonal1 || allEqual diagonal2)
+    allEqual diagonal1 || allEqual diagonal2
 
 
 checkColumn : List (List Char) -> Bool
 checkColumn board =
-    List.length (List.filter (\status -> status) <| List.map allEqual board) > 0
+    List.map allEqual board |> List.foldr (||) False
 
 
 checkRow : List (List Char) -> Bool
 checkRow board =
     let
-        rows =
-            List.indexedMap
-                (\index _ ->
-                    List.concat <|
+        rowsRes =
+            List.length board
+                |> List.range 0
+                |> List.map
+                    (\col ->
                         List.map
                             (\row ->
-                                takeElement index row
+                                takeElement col row ' '
                             )
                             board
-                )
-            <|
-                List.range 0 2
+                            |> allEqual
+                    )
     in
-        List.length (List.filter (\status -> status) <| List.map allEqual rows) > 0
+    rowsRes |> List.foldr (||) False
+
 
 updateCell : Model -> Int -> Int -> List (List Char)
 updateCell model posx posy =
-    (List.indexedMap
+    List.indexedMap
         (\y row ->
             List.indexedMap
                 (\x cell ->
                     if x == posx && y == posy && cell == ' ' then
                         model.turn
+
                     else
                         cell
                 )
                 row
         )
         model.board
-    )
+
+
 
 --View
 
+
 view : ( Model, Cmd msg ) -> Html Msg
-view (model, cmd) =
+view ( model, cmd ) =
     div []
         [ h1 [ class "titulo" ] [ text "Tic Tac Toe" ]
         , showWinner model
@@ -180,20 +209,25 @@ view (model, cmd) =
         , clearButton
         ]
 
+
 showWinner : Model -> Html Msg
 showWinner model =
     h1 []
         [ if model.winner then
-            text ("Winner = " ++ (String.fromChar model.turn))
+            text ("Winner = " ++ String.fromChar model.turn)
+
           else if model.draw then
             text "DRAW!"
+
           else
             text (String.fromChar model.turn ++ "'s Turn")
         ]
 
+
 clearButton : Html Msg
 clearButton =
     button [ type_ "button", onClick Clear ] [ text "Restart" ]
+
 
 makeBoard : Model -> Html Msg
 makeBoard model =
@@ -206,6 +240,7 @@ makeBoard model =
                 model.board
             )
         ]
+
 
 makeBoardCells : Int -> List Char -> Html Msg
 makeBoardCells y boardRow =
@@ -221,4 +256,6 @@ makeBoardCells y boardRow =
             boardRow
         )
 
-main = Browser.sandbox { init = initModel, update = update, view = view }
+
+main =
+    Browser.sandbox { init = initModel, update = update, view = view }
